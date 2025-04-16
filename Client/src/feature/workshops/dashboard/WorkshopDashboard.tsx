09b3294f713ae0913f483098e5e5f980b9aea7a1
lee-junmin <junminlee3@gmail.com>
2025-04-16 10:05:23 +1000

    make create workshop form

--- Client/src/feature/workshops/dashboard/WorkshopDashboard.tsx
+++ Client/src/feature/workshops/dashboard/WorkshopDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import WorkshopList from "./WorkshopList";
 import WorkshopDetails from "../details/WorkshopDetails";
+import WorkshopForm from "../forms/WorkshopForm";
 
 type Props = {
   workshops: Workshop[]
@@ -12,11 +13,12 @@ type Props = {
 export default function WorkshopDashboard({workshops, selectWorkshop, cancelSelectWorkshop, selectedWorkshop}: Props) {
   return (
     <Grid container spacing={3}>
-      <Grid size={4}>
+      <Grid size={6}>
           <WorkshopList workshops={workshops} selectWorkshop={selectWorkshop}/>
       </Grid>
-      <Grid size={8}>
+      <Grid size={6}>
          {selectedWorkshop && <WorkshopDetails workshop={selectedWorkshop} cancelSelectWorkshop={cancelSelectWorkshop} /> }
+         <WorkshopForm></WorkshopForm>
       </Grid>
     </Grid>
   )
---
+++ Client/src/feature/workshops/forms/WorkshopForm.tsx
@@ -0,0 +1,23 @@
+import { Box, TextField, Typography, Button, Paper } from "@mui/material";
+
+export default function WorkshopForm() {
+  return (
+    <Paper sx={{ borderRadius: 3, padding: 3 }}>
+    <Typography variant="h5" gutterBottom color="primary">
+        Create workshop
+    </Typography>
+    <Box component='form' display='flex' flexDirection='column' gap={3}>
+        <TextField name='title' label='Title'  />
+        <TextField name='description' label='Description'  multiline rows={3} />
+        <TextField name='category' label='Category'  />
+        <TextField name='date' label='Date' type="date"  />
+        <TextField name='city' label='City'  />
+        <TextField name='venue' label='Venue'  />
+        <Box display='flex' justifyContent='end' gap={3}>
+            <Button  color='inherit'>Cancel</Button>
+            <Button type="submit" color='success' variant="contained">Submit</Button>
+        </Box>
+    </Box>
+</Paper>
+  )
+}
